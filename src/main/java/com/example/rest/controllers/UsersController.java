eac1ac8116014c7bb77287386784f37fc1405aff
atarasov <atarasov@neoflex.ru>
2020-12-16 13:24:18 +0400

    Request validation

--- src/main/java/com/example/rest/controllers/UsersController.java
+++ src/main/java/com/example/rest/controllers/UsersController.java
@@ -2,10 +2,14 @@ package com.example.rest.controllers;
 
 import com.example.rest.services.User;
 import com.example.rest.services.UsersService;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/")
@@ -25,9 +29,15 @@ public class UsersController {
         return usersService.getAll();
     }
 
-    @PostMapping
-    public User addUser(@RequestParam String name){
-        return usersService.create(name);
+    @PostMapping(consumes = "application/json")
+    public User createUser(@RequestBody @Valid CreateUserRequest createUserRequest){
+        return usersService.create(createUserRequest.name);
+    }
+
+    @Data
+    static class CreateUserRequest{
+        @NotBlank
+        private String name;
     }
 
 }
