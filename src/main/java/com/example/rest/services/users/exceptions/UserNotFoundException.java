857ac2ff46d676044454cb5cb009093a2c9ca7f4
atarasov <atarasov@neoflex.ru>
2020-12-28 15:43:26 +0400

    Exceptions unused constructors

--- src/main/java/com/example/rest/services/users/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/rest/services/users/exceptions/UserNotFoundException.java
@@ -2,10 +2,6 @@ package com.example.rest.services.users.exceptions;
 
 public class UserNotFoundException extends RuntimeException{
 
-    public UserNotFoundException(Throwable cause) {
-        super(cause);
-    }
-
     public UserNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/example/rest/services/users/exceptions/UserParametersException.java
+++ src/main/java/com/example/rest/services/users/exceptions/UserParametersException.java
@@ -1,15 +1,9 @@
 package com.example.rest.services.users.exceptions;
 
 public class UserParametersException extends RuntimeException {
-    public UserParametersException(Throwable cause) {
-        super(cause);
-    }
 
     public UserParametersException(String message){
         super(message);
     }
 
-    public UserParametersException(String message, Throwable cause){
-        super(message, cause);
-    }
 }
