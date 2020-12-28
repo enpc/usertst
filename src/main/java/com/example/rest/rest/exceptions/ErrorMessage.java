438161c12e46ec9c9502c4bf45b89ee2b3723158
atarasov <atarasov@neoflex.ru>
2020-12-28 16:26:53 +0400

    REstExceptionHandler error messages.

--- src/main/java/com/example/rest/rest/exceptions/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package com.example.rest.rest.exceptions;
-
-import lombok.Data;
-
-@Data
-public class ErrorMessage {
-    private final Object message;
-}
--- src/main/java/com/example/rest/rest/exceptions/RestExceptionHandler.java
+++ src/main/java/com/example/rest/rest/exceptions/RestExceptionHandler.java
@@ -3,45 +3,38 @@ package com.example.rest.rest.exceptions;
 import com.example.rest.services.users.exceptions.UserNotFoundException;
 import com.example.rest.services.users.exceptions.UserParametersException;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Log4j2
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         var message = ex.getBindingResult().getFieldErrors().stream()
-                .map(s -> new Object(){
-                    public String field = s.getField();
-                    public String message = s.getDefaultMessage();
-                })
-                .collect(Collectors.toList());
-        return ResponseEntity.badRequest().body(new ErrorMessage(message));
+                .map(s -> s.getField() + " : " +s.getDefaultMessage())
+                .reduce("",(acc, l)->acc+"\n"+l);
+        return ResponseEntity.badRequest().body(message);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> notFoundExceptionHandler(UserNotFoundException ex, WebRequest request) {
+    public ResponseEntity<String> notFoundExceptionHandler() {
         return ResponseEntity.notFound().build();
     }
 
-    @ExceptionHandler(UserParametersException.class)
-    public ResponseEntity<Object> userParametersExceptionHandler(UserParametersException ex, WebRequest request) {
-        return ResponseEntity.badRequest().body(new ErrorMessage(ex.getMessage()));
+    @ExceptionHandler({UserParametersException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<String> userParametersExceptionHandler(Exception ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object>exceptionHandler(Exception ex){
-        logger.error(ex);
+    public ResponseEntity<Void>exceptionHandler(Exception ex){
+        log.error(ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
